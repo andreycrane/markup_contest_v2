a974fbedb7f0f5716cb7e6bf37563dea041dd7f6
Andrey Zhuravlev <crane.andrey@gmail.com>
2020-09-10 20:13:49 +0300

    fix: a part of job

--- posthtml.config.js
+++ posthtml.config.js
@@ -20,6 +20,32 @@ module.exports = {
           '../img/logos-airbnb.svg',
           '../img/logos-ibm.svg',
         ],
+        testimonials: [
+          {
+            name: 'Claire Bell',
+            prof: 'Designer',
+            descr: `I just wanted to share a quick note and let you know that you do a really good job. I'm glad I decided to work with you. Wonderful experience!`,
+            img: '../img/user-pic-Claire-Bell.png',
+          },
+          {
+            name: 'Philip Watson',
+            prof: 'Coder',
+            descr: 'You’re just awesome! The prices are reasonable, and the service is just great. Thank you so much! Highly recommend!',
+            img: '../img/user-pic-Francisco-Lane.png',
+          },
+          {
+            name: 'Ralph Fisher',
+            prof: 'Coder',
+            descr: 'A friend of mine advised you to me, and I definitely liked working with you. It was a good experience. Thanks a lot!',
+            img: '../img/user-pic-Jorge-Murphy.png',
+          },
+          {
+            name: 'Jorge Murphy',
+            prof: 'Designer',
+            descr: 'You did an amazing job and I wholeheartedly recommend you to everyone I know. You are the best!',
+            img: '../img/user-pic-Ralph-Fisher.png',
+          },
+        ],
       },
     },
   },
