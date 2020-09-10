8ac546735893c9eec63ec6e266715e20c8e8cdb0
Andrey Zhuravlev <crane.andrey@gmail.com>
2020-09-10 19:42:14 +0300

    another one part of job

--- posthtml.config.js
+++ posthtml.config.js
@@ -4,14 +4,22 @@ module.exports = {
     'posthtml-expressions': {
       locals: {
         gallery: [
-          "../img/gallery-image-1.png",
-          "../img/gallery-image-2.png",
-          "../img/gallery-image-3.png",
-          "../img/gallery-image-4.png",
-          "../img/gallery-image-5.png",
-          "../img/gallery-image-6.png",
-          "../img/gallery-image-7.png"
-        ]
+          '../img/gallery-image-1.png',
+          '../img/gallery-image-2.png',
+          '../img/gallery-image-3.png',
+          '../img/gallery-image-4.png',
+          '../img/gallery-image-5.png',
+          '../img/gallery-image-6.png',
+          '../img/gallery-image-7.png',
+        ],
+        partners: [
+          '../img/logos-apple-app-store.svg',
+          '../img/logos-apiary.svg',
+          '../img/logos-android-icon.svg',
+          '../img/logos-basecamp.svg',
+          '../img/logos-airbnb.svg',
+          '../img/logos-ibm.svg',
+        ],
       },
     },
   },
