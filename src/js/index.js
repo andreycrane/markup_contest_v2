d43d65b961e656d6a07e4421dc0ee2b67ebe86f1
Andrey Zhuravlev <crane.andrey@gmail.com>
2020-09-22 20:41:00 +0300

    wip: работа на мобильным меню сайта

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 (function () {
- //  your code here
+  //  your code here
+  const topContainerEl = document.querySelector('.js-top-container');
+  const menuBtnEl = document.querySelector('.js-menu-btn');
+
+  menuBtnEl.addEventListener('click', () => {
+    topContainerEl.classList.toggle('header-section__top-container--menu-open');
+    document.body.classList.toggle('scroll-lock');
+  });
 })();
